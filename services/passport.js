5488b839dd551f4881eeb02f540029b4720bc63b
JR-SHI <Jean-robert_rene@shi.com>
2020-12-10 13:00:36 -0500

    auth flow

---
+++ services/passport.js
@@ -0,0 +1,44 @@
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user)
+    });
+});
+
+passport.use(
+  new googleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token: ", accessToken);
+      console.log("refresh token: ", refreshToken);
+      console.log("profile: ", profile);
+      User.findOne({
+        googleID: profile.id
+      }).then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser)
+        } else {
+          // We dont have a user with this ID, make a new record
+          new User({
+            googleID: profile.id
+          }).save().then(user => done(null, user));
+        }
+      })
+    }
+  )
+);
