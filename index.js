49152d556a9c4398432ce0434fe42eecc195d57a
JR-SHI <Jean-robert_rene@shi.com>
2020-12-28 11:04:44 -0500

    added billing and clientside app

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieSession = require('cookie-session');
+const cookieSession = require("cookie-session");
 const passport = require("passport");
-const keys = require('./config/keys');
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
 
@@ -10,14 +11,28 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.use(cookieSession({
-  maxAge: 30 * 24 * 60 * 60 * 1000,
-  keys: [keys.cookieKey]
-}));
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  //express will serve up production assets, like main.js or main.css files
+  app.use(express.static("client/build"));
+  //express will; serve up the index.html file if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //const Schema = mongoose.Schema;
-const {
-  Schema
-} = mongoose;
+const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleID: String
-})
+  googleID: String,
+  credits: { type: Number, default: 0 },
+});
 
-mongoose.model('users', userSchema)
+mongoose.model("users", userSchema);
